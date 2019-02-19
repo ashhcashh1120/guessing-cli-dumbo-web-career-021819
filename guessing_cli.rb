b608ebe5d9111f856ae4e17dd85bc632cff2e90d
Ashley Archibald <aarchibald20@gmail.com>
2019-02-19 03:20:54 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,18 @@
 # Code your solution here!
+def run_guessing_game
+  loop do
+     puts "Guess a number between 10 and 20."
+     input = gets.chomp
+     computer = rand(10..20)
+     if input == "exit"
+       puts "Goodbye!"
+       break
+     elsif input.to_i == computer
+         puts "You guessed the correct number!"
+       elsif input.to_i != computer
+         puts "The computer guessed #{computer}."
+       else
+         puts "Invalid input"
+       end
+     end
+   end
